cd13842b1216d3ebb1612a32ead0178e8f304931
Justinkuo1053 <justinkuo719@gmail.com>
2024-01-18 13:42:35 +0800

    2

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
 const tasks = require("./routes/tasks");
-const dotenv = require("dotenv");
-dotenv.config();
+const connectDB = require("./db/connect");
+require("dotenv").config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const cors = require("cors");
@@ -25,19 +23,10 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("連結到mongodb...");
-  })
-  .catch((e) => {
-    console.log(e);
-    return res.status(500).send("無法儲存使用者。。。");
-  });
-
 const start = async () => {
   try {
-    await app.listen(port, () =>
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
